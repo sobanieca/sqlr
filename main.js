876d17112d553d3ba910aed46acdaf4c4cd0dbad
Adam Sobaniec <sobanieca@gmail.com>
2022-10-05 23:00:33 +0200

    todo updated

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.2/command/mod.ts";
 
 await new Command()
-  .name("cliffy")
+  .name("sqlr")
   .version("0.1.0")
-  .description("Command line framework for Deno")
+  .description("Command line for executing SQL queries")
+  .option("-c, --connection <connection:string>", "Connection to be used")
+  .globalOption("--debug", "Enable debug logs")
   .parse(Deno.args);
 
 
